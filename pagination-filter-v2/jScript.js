319e9067388f86fea916a31210db970aecafc050
shaun deshler <shaundeshler@shauns-MacBook-Pro.local>
2018-04-18 17:39:22 -0700

    added live search

--- pagination-filter-v2/jScript.js
+++ pagination-filter-v2/jScript.js
@@ -1,24 +1,42 @@
 const people = document.querySelectorAll('h3');
+const ul = document.querySelector('ul');
+const li = document.querySelectorAll('li');
 const search = document.createElement("input");
 const inputParent = document.querySelector('.page-header');
 inputParent.appendChild(search);
-let person;
 let arr = [];
 
 const listOfPeople = () => {
-  let one;
-for(x in people){
-  if(typeof people[x].innerHTML === "string"){
-    one = people[x].innerHTML;
-    arr.push(one);
+  for(x in people){
+    if(typeof people[x].innerHTML === "string"){
+      arr.push(people[x].innerHTML);
+    }
+  }
+  return arr;
+}
+
+listOfPeople();
+
+const buttonList = () => {
+  for(x in li){
+    let pages = parseInt(x) % 10;
+    if(typeof li[x] === "object" && li[x].style.display === "block"){
+        
+    }
   }
 }
-return arr;
-}
-
-console.log(listOfPeople())
 
 search.addEventListener('keyup', (e) => {
-    person = e.target.value;
-    console.log(listOfPeople().indexOf(person))
-})
+    const person = e.target.value;
+    arr.map((c,i) => {
+      if(c.indexOf(person) > -1){
+        li[i].style.display = "block";
+      }
+      else {
+        li[i].style.display = "none";
+        }
+      }
+    )
+    buttonList();
+  }
+)
